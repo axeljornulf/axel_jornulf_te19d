f2424d76dc9c9dea97b7167636478b4f26583e98
axeljornulf <axel.jornulf@elev.ga.ntig.se>
2020-08-31 10:36:23 +0200

    Skapat med variabler, gjort input/output.

---
+++ variabler/variabler.py
@@ -0,0 +1,11 @@
+name = "Axel" #skapat variabel med stränget "Axel"
+age = 19 #skapat variabel med värdet 19
+print(f"Hej {name}, du är {age} år gammal")
+
+side = float(input("Ange kvadratens sida: "))
+
+area = side**2
+
+omkrets = 4*side
+
+print(f"Kvadratens area är {area} a.e. och omkretsen är {omkrets} l.e.")
