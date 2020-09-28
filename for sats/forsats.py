ff34048e7800e830bd9aa06ed78a01ef4753a77b
axeljornulf <axel.jornulf@elev.ga.ntig.se>
2020-09-28 09:48:33 +0200

    uppgifter

---
+++ for sats/forsats.py
@@ -0,0 +1,11 @@
+s = 0
+
+for i in range(1,10,2):
+    s += i #s = s + i
+
+print(s)
+
+# för += i blir s = 0, s = 0 + 1, s = 1 + 2, s = 3 + 3, s = 6 + 4
+# s = 0+1+2+3+...+9
+# 2 = steg
+# += i blir s = 2
